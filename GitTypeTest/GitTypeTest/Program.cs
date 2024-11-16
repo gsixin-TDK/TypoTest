8905652066153cd6ad3c4a034b66e04798ef21d7
gsixin <sixin.gao@accunome.com.cn>
2024-11-16 21:12:43 +0800

    update

--- GitTypeTest/GitTypeTest/Program.cs
+++ GitTypeTest/GitTypeTest/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("BBTest");
+            Console.WriteLine("braTest");
             Console.ReadLine();
         }
     }
